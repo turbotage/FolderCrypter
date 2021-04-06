632e5e124857a5869f5d04b93c37c75ac37d8d0f
turbotage <viktor.vigrennaslund@live.se>
2021-04-06 02:35:21 +0200

    fixed file separation and changed to AES256-GCM

--- ter.py
+++ ter.py
@@ -12,10 +12,12 @@ import stdiomask
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+#from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
-from cryptography.fernet import Fernet, InvalidToken
-from cryptography.exceptions import InvalidKey
+#from cryptography.fernet import Fernet, InvalidToken
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+from cryptography.exceptions import InvalidKey, InvalidTag
 
 
 # Print iterations progress
@@ -43,14 +45,14 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 
 def append_folder_padding_files(directory):
 	for subdir, _, _ in os.walk(directory):
-		filename = subdir + os.sep + "f8fae9b3-8227-4505-aef1-922b701ce0b7-5726e5ec-9681-4884-9f1b-399bddf0a73c.padfile"
+		filename = "f8fae9b3-8227-4505-aef1-922b701ce0b7-5726e5ec-9681-4884-9f1b-399bddf0a73c.padfile"
 
-		with open(filename, 'wb') as f:
+		with open(subdir + os.sep + filename, 'wb') as f:
 			filesize = random.randint(400, 8000)
 			letters = string.ascii_lowercase + string.ascii_uppercase + '0123456789'
 			result_str = ''.join(random.choice(letters) for i in range(filesize)).encode()
 
-			result_str +=  b'FILENAME:' + filename.encode()
+			result_str +=  b'DIR:' + subdir.encode() + b'FILENAME:' + filename.encode()
 
 			f.write(result_str)
 
@@ -70,7 +72,7 @@ def get_key(verbose_pass=False):
 
 	# Okey for this application, if this application is used for storring multiple users data or something like that
 	# A non static salt should be used to avoid rainbow-table vulnerbilities
-	salt = b'\xa4\xb4\x11\xde\x05<\xdck\xc1\xfc6R\xaf\x97\xa1j'
+	salt = b'u\xcf(\n\xd5\x9c\x05\xffy\x97\x96\xb1@\x1f\rn'
 
 	time1 = time.time()
 
@@ -79,8 +81,8 @@ def get_key(verbose_pass=False):
 	"""
 	kdf = Scrypt(salt = salt, length=32, n = 2**21, r=8, p=1)
 
-	key = base64.urlsafe_b64encode(kdf.derive(password1))
-	#key = kdf.derive(password1)
+	#key = base64.urlsafe_b64encode(kdf.derive(password1))
+	key = kdf.derive(password1)
 
 	print("It took ", time.time() - time1, " seconds to generate the key")
 
@@ -96,7 +98,9 @@ def get_key(verbose_pass=False):
 
 
 def encrypt(input_file_names, key):
-	fernet = Fernet(key)
+	#fernet = Fernet(key)
+	aesgcm = AESGCM(key)
+
 
 	i = 0
 	l = len(input_file_names)
@@ -106,15 +110,22 @@ def encrypt(input_file_names, key):
 		if i % 4 == 0:
 			printProgressBar(i, l, prefix='Progress:', suffix='Complete', length = 50)
 
-		with open(input_file_name, 'rb') as fin:
+		with open(input_file_name[0] + os.sep + input_file_name[1], 'rb') as fin:
 			data = fin.read() # Read the bytes of the input file
 			
-			data += b'FILENAME:' + input_file_name.encode()
+			data += b'DIR:' + input_file_name[0].encode() + b'FILENAME:' + input_file_name[1].encode()
+			
+			nonce = os.urandom(12)
+			auth = str(uuid.uuid4()).encode()
 
-			encrypted_data = fernet.encrypt(data)
+			encrypted_data = aesgcm.encrypt(nonce, data, auth)
+
+			encrypted_data += b'NC:' + nonce + b'AUTH:' + auth
 
 			# Generate random filename
 			encrypted_file_name = "Encrypted" + os.sep + str(uuid.uuid4())
+			while (os.path.exists(encrypted_file_name)): # Chances for this happening are astronomically small
+				encrypted_file_name = "Encrypted" + os.sep + str(uuid.uuid4())
 
 			os.makedirs(os.path.dirname(encrypted_file_name), exist_ok=True)
 			with open(encrypted_file_name, 'wb') as fout:
@@ -136,7 +147,7 @@ def encryption():
 	input_file_names = []
 	for subdir, _, files in os.walk(folder_to_encrypt):
 		for filename in files:
-			input_file_names.append(subdir + os.sep + filename)
+			input_file_names.append((subdir,filename))
 
 	key = get_key()
 
@@ -146,9 +157,8 @@ def encryption():
 
 
 def decrypt(input_file_names, encryption_folder_name, key):
-	fernet = Fernet(key)
+	aesgcm = AESGCM(key)
 
-	root_directory_name = None
 	i = 0
 	l = len(input_file_names)
 	printProgressBar(0, l, prefix='Progress:', suffix='Complete', length = 50)
@@ -160,26 +170,33 @@ def decrypt(input_file_names, encryption_folder_name, key):
 		with open(encryption_folder_name + os.sep + input_file_name, 'rb') as fin:
 			data = fin.read() # Read the bytes of the input file
 
+			encrypted_data, auth = data.rsplit(b'AUTH:', 1)
+			encrypted_data, nonce = encrypted_data.rsplit(b'NC:', 1)
+
+			decrypted_data = None
 			try:
-
-				decrypted_data = fernet.decrypt(data)
-
-				decrypted_data, decrypted_file_name = decrypted_data.rsplit(b'FILENAME:', 1)
-				
-				decrypted_file_name = decrypted_file_name.decode()
-
-				# Ignore padding files
-				if "f8fae9b3-8227-4505-aef1-922b701ce0b7-5726e5ec-9681-4884-9f1b-399bddf0a73c.padfile" in decrypted_file_name:
-					os.makedirs(os.path.dirname(decrypted_file_name), exist_ok=True)
-					continue
-
-				os.makedirs(os.path.dirname(decrypted_file_name), exist_ok=True)
-				with open(decrypted_file_name, 'wb') as fout:
-					fout.write(decrypted_data)
-
-			except InvalidToken as e:
-				print("Invalid key - Unsuccessfully decrypted")
+				decrypted_data = aesgcm.decrypt(nonce, encrypted_data, auth)
+			except InvalidTag:
+				print("Invalid auth, nonce, or key")
 				exit(-1)
+
+
+			decrypted_data, filename = decrypted_data.rsplit(b'FILENAME:', 1)
+			decrypted_data, dir_name = decrypted_data.rsplit(b'DIR:', 1)
+			filename = filename.decode()
+			dir_name = dir_name.decode()
+
+			decrypted_file_path = dir_name + os.sep + filename
+
+			os.makedirs(os.path.dirname(decrypted_file_path), exist_ok=True)
+			# Ignore padding files
+			if "f8fae9b3-8227-4505-aef1-922b701ce0b7-5726e5ec-9681-4884-9f1b-399bddf0a73c.padfile" == filename:
+				continue
+
+			with open(decrypted_file_path, 'wb') as fout:
+				fout.write(decrypted_data)
+
+
 
 	printProgressBar(l, l, prefix='Progress:', suffix='Complete', length = 50)
 
